70e68bebd467ce8a5588d442d9e1ff2d2c7dd510
Julio Da silva <julio.gdsilva@gmail.com>
2024-07-10 02:52:12 -0300

    add job buildings

--- src/jobs/buildings.js
+++ src/jobs/buildings.js
@@ -23,8 +23,9 @@ module.exports = (app) => {
       const getCoordinates = axios(`https://brasilapi.com.br/api/cep/v2/${building.cep}`)
         .then()
         .catch((err) => {
-          if (err.response.status === '404') return;
-          throw err;
+          if (err.resp.status === '404') return;
+          console.log(err);
+          // throw err;
         });
 
       const latitude = getCoordinates?.location?.coordinates?.latitude;
