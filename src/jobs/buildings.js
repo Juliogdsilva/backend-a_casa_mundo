b89cbe90550e9558daad1e3a044c199c0170c7b5
Julio Da silva <julio.gdsilva@gmail.com>
2024-07-10 02:59:33 -0300

    add job buildings

--- src/jobs/buildings.js
+++ src/jobs/buildings.js
@@ -23,7 +23,7 @@ module.exports = (app) => {
         .then()
         .catch((err) => {
           // if (err.resp.status === '404') return;
-          console.log(err.response);
+          console.log(err.response.res);
           // throw err;
         });
 
