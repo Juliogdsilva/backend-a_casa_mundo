08e22a3baf42bc74c5223ff25c7ac3f7e6450c0e
Julio Da silva <julio.gdsilva@gmail.com>
2024-07-10 02:34:17 -0300

    add job buildings

--- src/jobs/buildings.js
+++ src/jobs/buildings.js
@@ -15,31 +15,30 @@ module.exports = (app) => {
         throw err;
       });
 
-    // eslint-disable-next-line operator-assignment
-    for (let i = 0; i < buildings.length; i = i + 1) {
+    for (let i = 0; i < buildings.length; i += 1) {
       const building = buildings[i];
       console.log(i);
 
-      const getCoordinates = await axios(`https://brasilapi.com.br/api/cep/v2/${building.cep}`)
-        .then()
-        .catch((err) => {
-          throw err;
-        });
+      // const getCoordinates = await axios(`https://brasilapi.com.br/api/cep/v2/${building.cep}`)
+      //   .then()
+      //   .catch((err) => {
+      //     throw err;
+      //   });
 
-      const latitude = getCoordinates?.location?.coordinates?.latitude;
-      const longitude = getCoordinates?.location?.coordinates?.longitude;
+      // const latitude = getCoordinates?.location?.coordinates?.latitude;
+      // const longitude = getCoordinates?.location?.coordinates?.longitude;
 
-      if (latitude && longitude) {
-        const date = new Date();
-        app
-          .db('buildings')
-          .update({ latitude, longitude, updated_at: date })
-          .where({ id: building.id })
-          .then()
-          .catch((err) => {
-            throw err;
-          });
-      }
+      // if (latitude && longitude) {
+      //   const date = new Date();
+      //   app
+      //     .db('buildings')
+      //     .update({ latitude, longitude, updated_at: date })
+      //     .where({ id: building.id })
+      //     .then()
+      //     .catch((err) => {
+      //       throw err;
+      //     });
+      // }
     }
   });
 };
