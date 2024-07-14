5226305eb09eb13a1439349ac024a281bc1a637a
Julio Da silva <julio.gdsilva@gmail.com>
2024-07-14 16:49:41 -0300

    update lat e lng

--- src/jobs/buildings.js
+++ src/jobs/buildings.js
@@ -4,12 +4,14 @@ const axios = require('axios');
 
 module.exports = (app) => {
   cron.schedule('*/5 * * * *', async () => {
+    const date = new Date();
     const buildings = await app
       .db('buildings')
       .select('id', 'cep')
       .where('latitude', null)
       .orWhere('longitude', null)
       .whereNot('cep', null)
+      .whereNot({ not_found_brasil_api: 1, not_found_ibge_api: 1 })
       .whereNot('status', 'deleted')
       .limit(2)
       .then()
@@ -20,10 +22,21 @@ module.exports = (app) => {
     for (let i = 0; i < buildings.length; i += 1) {
       const building = buildings[i];
 
+      // eslint-disable-next-line no-await-in-loop
       let getCoordinates = await axios(`https://brasilapi.com.br/api/cep/v2/${building.cep}`)
         .then()
         .catch((err) => {
-          if (err.response?.status === 404) return;
+          if (err.response?.status === 404) {
+            app
+              .db('buildings')
+              .update({ not_found_brasil_api: 1, updated_at: date })
+              .where({ id: building.id })
+              .then()
+              .catch((err) => {
+                throw err;
+              });
+            return;
+          }
           if (err.response?.status === 524) return;
           throw err;
         });
@@ -31,7 +44,6 @@ module.exports = (app) => {
       let longitude = getCoordinates?.data?.location?.coordinates?.longitude;
 
       if (latitude && longitude) {
-        const date = new Date();
         app
           .db('buildings')
           .update({ latitude, longitude, updated_at: date })
@@ -43,10 +55,21 @@ module.exports = (app) => {
         continue;
       }
 
+      // eslint-disable-next-line no-await-in-loop
       getCoordinates = await axios(`https://cep.awesomeapi.com.br/json/${building.cep}`)
         .then()
         .catch((err) => {
-          if (err.response?.status === 404) return;
+          if (err.response?.status === 404) {
+            app
+              .db('buildings')
+              .update({ not_found_ibge_api: 1, updated_at: date })
+              .where({ id: building.id })
+              .then()
+              .catch((err) => {
+                throw err;
+              });
+            return;
+          }
           if (err.response?.status === 524) return;
           throw err;
         });
@@ -54,7 +77,6 @@ module.exports = (app) => {
       longitude = getCoordinates?.data?.lng;
 
       if (latitude && longitude) {
-        const date = new Date();
         app
           .db('buildings')
           .update({ latitude, longitude, updated_at: date })
